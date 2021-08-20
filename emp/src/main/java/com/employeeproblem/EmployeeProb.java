6bf1582ab63d17c22c0eca43ef6cae7428d1a62b
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-20 23:30:50 +0530

    Added:Daily wage calculation on basis of attendance

--- emp/src/main/java/com/employeeproblem/EmployeeProb.java
+++ emp/src/main/java/com/employeeproblem/EmployeeProb.java
@@ -4,12 +4,21 @@ public class EmployeeProb {
 
 	public static void main(String[] args) 
 	{
-		int IS_ATTENDANCE=1;
+		int IS_ATTENDANCE=1,IS_Wage_Per_Hour=20,IS_Full_Day=8;
+		int daily_employee_wage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(IS_ATTENDANCE==empCheck)
+		{
 			System.out.println("Employee is Present");
+			daily_employee_wage=IS_Wage_Per_Hour*IS_Full_Day;
+			System.out.println("Employee Daily wage is:"+daily_employee_wage);
+		}
 		else
+		{
 			System.out.println("Employee is Absent");
+			daily_employee_wage=IS_Wage_Per_Hour*0;
+			System.out.println("Since employee is absent so his daily wage:"+daily_employee_wage);
+		}
 
 	}
 
