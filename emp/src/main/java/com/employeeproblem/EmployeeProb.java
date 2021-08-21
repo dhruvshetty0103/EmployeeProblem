de949de03ce3e6f044a7b8172ec5cf3bbd488600
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-21 13:07:41 +0530

    Added:Wage will be calculated on the basis working hours where the condition is that max working hours can be 100 or working days upto 20 days

--- emp/src/main/java/com/employeeproblem/EmployeeProb.java
+++ emp/src/main/java/com/employeeproblem/EmployeeProb.java
@@ -9,26 +9,41 @@ public class EmployeeProb
 
 	public static void main(String[] args) 
 	{
-		int daily_employee_wage=0,monthly_wage=0;
-		int empCheck=(int)(Math.floor(Math.random()*10)%3);//To generate random values in range of 0-2 where a floor will ignore decimal part
-		switch(empCheck)
+		int daily_employee_wage=0,monthly_wage=0,working_hours=0,working_days=0;
+		
+		while(working_hours<100 && working_days<20)
 		{
-		case 1:System.out.println("When employee works Full day throughout the month");
-			   daily_employee_wage=IS_WAGE_PER_HOUR*IS_FULL_DAY_HOURS;//Calculation of monthly wage
-			   monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
-			   System.out.println("Employee monthly wage is:"+monthly_wage);
-			   break;
-		case 2:System.out.println("When employee works Part time a day throughout the month");
-		   	   daily_employee_wage=IS_WAGE_PER_HOUR*IS_PART_TIME_HOURS;//Calculation of monthly wage
-		   	   monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
-		   	   System.out.println("Employee monthly wage is:"+monthly_wage);
-		   	   break;
-		default:System.out.println("When an employee is Absent throughout the month");
-			    daily_employee_wage=IS_WAGE_PER_HOUR*0;
-			    monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
-			    System.out.println("His monthly wage:"+monthly_wage);
-			    break;
+			int empCheck=(int)(Math.floor(Math.random()*10)%3);//To generate random values in range of 0-2 where a floor will ignore decimal part
+			switch(empCheck)
+			{
+			case 1://System.out.println("When employee works Full day throughout the month");
+				   daily_employee_wage=IS_WAGE_PER_HOUR*IS_FULL_DAY_HOURS;//Calculation of monthly wage
+				   monthly_wage+=daily_employee_wage;
+				   working_hours+=8;//Adding 8 working hours when full day
+				   working_days+=1;//Adding 1 day every time irrespective of type of attendance
+				   
+				   break;
+			case 2://System.out.println("When employee works Part time a day throughout the month");
+			   	   daily_employee_wage=IS_WAGE_PER_HOUR*IS_PART_TIME_HOURS;//Calculation of monthly wage
+			   	   monthly_wage+=daily_employee_wage;
+			   	   working_hours+=4;//Adding 4 working hours when part time
+				   working_days+=1;//Adding 1 day every time irrespective of type of attendance
+			   	   
+			   	   break;
+			default://System.out.println("When an employee is Absent throughout the month");
+				    daily_employee_wage=IS_WAGE_PER_HOUR*0;
+				    monthly_wage+=daily_employee_wage;
+				    working_hours+=0;//Adding 0 working hours when absent
+					working_days+=1;//Adding 1 day every time irrespective of type of attendance
+				    
+				    break;
+			}
 		}
+		System.out.println("Employee monthly wage is:"+monthly_wage);//Calculating monthly basis depending on numbers of hours worked where per hour costs 20 
+		if(working_hours==100 && working_days!=20)//Condition to check which is reached first working hours or days
+			System.out.println("Number of working hours reached first:"+working_hours+" within:"+working_days+" working days.");
+		else
+			System.out.println("Number of working days reached first:"+working_days+" with:"+working_hours+" working hours.");
 
 	}
 
