88c893b99f470e637d410e1d65740cc5c95b09af
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-21 12:27:53 +0530

    Added:Calculating momthly wage of employee depending he is doing Full time,Part time and absent throughout the month

--- emp/src/main/java/com/employeeproblem/EmployeeProb.java
+++ emp/src/main/java/com/employeeproblem/EmployeeProb.java
@@ -2,28 +2,31 @@ package com.employeeproblem;
 
 public class EmployeeProb 
 {
-	public static final int IS_WAGE_PER_HOUR=20,IS_FULL_DAY_HOURS=8,IS_PART_TIME_HOURS=4;
+	public static final int IS_WAGE_PER_HOUR=20,IS_FULL_DAY_HOURS=8,IS_PART_TIME_HOURS=4,IS_MONTHLY_DAYS=20;
 	//IS_FULL_TIME=1,IS_PART_TIME=2 removed these variables to implement switch case
 	//Here above are global variables where full time and part time are values to show nature of attendance
 	//and full day hours and part time hours are nothing but number of hours worked per day.
 
 	public static void main(String[] args) 
 	{
-		int daily_employee_wage=0;
+		int daily_employee_wage=0,monthly_wage=0;
 		int empCheck=(int)(Math.floor(Math.random()*10)%3);//To generate random values in range of 0-2 where a floor will ignore decimal part
 		switch(empCheck)
 		{
-		case 1:System.out.println("Employee is Full day Present");
-			   daily_employee_wage=IS_WAGE_PER_HOUR*IS_FULL_DAY_HOURS;//Calculation of daily wage
-			   System.out.println("Employee Daily wage is:"+daily_employee_wage);
+		case 1:System.out.println("When employee works Full day throughout the month");
+			   daily_employee_wage=IS_WAGE_PER_HOUR*IS_FULL_DAY_HOURS;//Calculation of monthly wage
+			   monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
+			   System.out.println("Employee monthly wage is:"+monthly_wage);
 			   break;
-		case 2:System.out.println("Employee is Part time Present");
-			   daily_employee_wage=IS_WAGE_PER_HOUR*IS_PART_TIME_HOURS;
-			   System.out.println("Employee Daily wage is:"+daily_employee_wage);
-			   break;
-		default:System.out.println("Employee is Absent");
+		case 2:System.out.println("When employee works Part time a day throughout the month");
+		   	   daily_employee_wage=IS_WAGE_PER_HOUR*IS_PART_TIME_HOURS;//Calculation of monthly wage
+		   	   monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
+		   	   System.out.println("Employee monthly wage is:"+monthly_wage);
+		   	   break;
+		default:System.out.println("When an employee is Absent throughout the month");
 			    daily_employee_wage=IS_WAGE_PER_HOUR*0;
-			    System.out.println("Since employee is absent so his daily wage:"+daily_employee_wage);
+			    monthly_wage=daily_employee_wage*IS_MONTHLY_DAYS;
+			    System.out.println("His monthly wage:"+monthly_wage);
 			    break;
 		}
 
